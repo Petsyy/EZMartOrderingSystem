a898557272a6a8f23640155b7b5ce669948036f9
Petsyy <peterarenasdiaz16@gmail.com>
2025-08-21 20:27:41 +0800

    initial commit

--- api/fetch_users.php
+++ api/fetch_users.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/db.php'; 
+require_once __DIR__ . '/../config/db.php';
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
@@ -24,4 +24,3 @@ try {
     http_response_code(500);
     echo json_encode(['success' => false, 'message' => 'Database error. Please try again later.']);
 }
-?>
